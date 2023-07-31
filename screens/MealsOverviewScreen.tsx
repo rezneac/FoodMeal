ef949817d6a0b0cd4255283a141b4c4012ebd061
rezneac <rezneacanatolie@gmail.com>
2023-07-31 12:57:49 +0100

    Adding Meals items and title with additional information

--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,9 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 import {MEALS} from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 interface IProps {
   navigation: object;
-  route: {params: {categoryId: string}};
+  route: any;
 }
 
 const MealsOverviewScreen = ({navigation, route}: IProps) => {
@@ -13,9 +14,22 @@ const MealsOverviewScreen = ({navigation, route}: IProps) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
 
+  function renderMealItem(itemData: any) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList data={displayedMeals} keyExtractor={item => item.id} renderItem={renderMealItem} />
     </View>
   );
 };
